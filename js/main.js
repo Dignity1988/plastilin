52638521c476a874f75ede8855aec5464967c4c3
Dignity1988 <maksym.malin88@gmail.com>
2018-06-04 05:10:02 +0300

    adaptive fixes
    The mobile layout reached in responsive manner but  the option to make separate still looks so much more attractive

--- js/main.js
+++ js/main.js
@@ -10,11 +10,12 @@ jQuery(document).ready(function(){
 
   // the magic
   responsive: [{
-  arrows: false,
+
       breakpoint: 768,
       settings: {
         slidesToShow: 1,
-        dots: true
+        dots: true,
+          arrows: false,
       },
 
     }]  });
@@ -35,8 +36,59 @@ arrows: false,
     breakpoint: 768,
     settings: {
       slidesToShow: 1,
+        arrows: false,
       dots: true
     },
 
   }]  });
-});
+
+
+
+    /* the non-finished logic of handmade slider
+  var slides=$(".slides ").length();
+  var slideWidth=$(".slide").height();
+  Jquery('arrow-right').on('click', function(e){
+  $('slide').position(left+slidewidth);
+    /*count all elements in slider*/
+  /* width+() outer width of the next node element within list
+  if click counter is reached the last element of the node list reset counter and start it with first outer width multiply value.
+
+  var dot="<li class="dot"></li>";
+  var arrow="<div class="arrow"></div>";*/
+  /*if browser width is less than 768px*/
+  if (window.matchMedia('(max-width:768px)').matches){
+    $('.heading-nav').click (function (e) {
+        e.preventDefault();
+  $('.slider-1').slick('slickGoTo', jQuery(this).attr('data-index'));
+    });
+  /*add the dots*/
+  $('.full').attr('src', './img/build/top_mobile.png');
+  $('.mobile').css('display', 'block');
+  $('h1').css('display', 'block');
+  }
+  else{
+  $('.full').attr('src', './img/build/top_row.png');
+  $('.mobile').css('display', 'none');
+  $('h1').css('display', 'none');
+  }
+  jQuery(window).resize(function(){
+    $('.heading-nav').click (function (e) {
+        e.preventDefault();
+  $('.slider-1').slick('slickGoTo', jQuery(this).attr('data-index'));
+    });
+  if (window.matchMedia('(max-width:768px)').matches){
+  /*add the dots*/
+  $('.full').attr('src', './img/build/top_mobile.png');
+  $('.mobile').css('display', 'block');
+  $('h1').css('display', 'block');
+  }
+  else{
+  $('.full').attr('src', './img/build/top_row.png');
+  $('.mobile').css('display', 'none');
+  $('h1').css('display', 'none');
+  }
+
+  });
+
+
+            });
